9457c964c403f8005415008954217e0c167df86b
alexgdi <alexgdi@outlook.com>
2021-03-05 14:19:09 +0300

    initialize commit

---
+++ GopApp.c
@@ -0,0 +1,12 @@
+#include <Uefi.h>
+#include <Library/PcdLib.h>
+#include <Library/UefiLib.h>
+#include <Library/UefiApplicationEntryPoint.h>
+
+// The Entry Point for Application. The user code starts with this function
+EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable)
+{
+	//
+
+	return EFI_SUCCESS;
+}
